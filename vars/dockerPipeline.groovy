e5dcc35a0a3dc1293c549bb1bd0adf48e97b6898
Avej K Lanjekar <avej.lanjekar11@gmail.com>
2026-09-17 08:18:34 +0000

    add push stage to shared library

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,4 +1,12 @@
 def call(String imagename){
     echo "Building docker image ${imagename}"
     def app= docker.build("${imagename}:${GIT_COMMIT}")
+
+    docker.withRegistry(
+        'https://registry.hub.docker.com',
+        'dockerhub-credentials'
+    )
+    {
+        app.push()
+    }
 }
